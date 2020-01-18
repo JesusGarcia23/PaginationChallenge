33904b6de1fe290186a8633bbf5b0e71ac9b6570
= <jacex17@gmail.com>
2020-01-18 18:28:47 -0500

    fix file structure for deployment

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const logger  = require('morgan');
 const path = require('path');
 const data = require('./data');
 const app = express();
-const PORT = 5000;
 
 // Middleware Setup
 app.use(logger('dev'));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('index');
+    res.redirect('http://localhost:5000/apps');
 })
 
 app.get('/apps', (req, res) => {
@@ -89,7 +85,12 @@ app.get('/apps', (req, res) => {
     res.json(allData)
 })
 
-app.listen(PORT, console.log(`SERVER LISTENING ON ${PORT}`))
+app.get((req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/index.html");
+  });
+
+module.exports = app;
 
 
 //ALL TESTS
@@ -107,90 +108,3 @@ app.listen(PORT, console.log(`SERVER LISTENING ON ${PORT}`))
 //http://localhost:5000/apps?by=id&start=1&order=desc
 //http://localhost:5000/apps?by=id&start=5&end=10&max=10&order=asc
 
-
-    // if(by === 'id') {
-
-    //  limit = max || dataLength; 
-
-    //  theOrder = order === 'desc' ? 'desc' : 'asc';
-
-    //  startIndex = start > 0 ? start - 1 : 0
-
-    //  lastIndex = limit < end ? limit : end
-
-    //  allData = allData.slice(startIndex, lastIndex).slice(0, limit);
-
-    //  switch(theOrder){
-        
-    //     case 'desc': {
-    //         allData = allData.sort((a, b) => {
-    //             if (b[by] > a[by]) {
-    //                 return 1;
-    //             }else if(b[by] < a[by]) {
-    //                 return -1;
-    //             }else{
-    //                 return 0;
-    //             }
-    //         })
-    //         break;
-    //     }
-
-    //     default: {
-    //        allData = allData.sort((a, b) => {
-    //             if (a[by] > b[by]) {
-    //                 return 1;
-    //             }else if(a[by] < b[by]) {
-    //                 return -1;
-    //             }else{
-    //                 return 0;
-    //             }
-    //         })
-    //     }
-    // }
-
-    // }else if(by === 'name') {
-
-    //  limit = max > 0 ? max : dataLength;
-
-    //  const nameStartIndex = allData.findIndex(dataToSearch => dataToSearch.name === start);
- 
-    //  const nameLastIndex = allData.findIndex(dataToSearch => dataToSearch.name === end);
-
-    //  theOrder = order === 'desc' ? 'desc' : 'asc';
-
-    //  startIndex = nameStartIndex >= 0 ? nameStartIndex : 0;
-
-    //  lastIndex = nameLastIndex >= 0 ? nameLastIndex : limit;
-
-    //  allData = allData.slice(startIndex, lastIndex + 1).slice(0, limit);
-
-
-    //  switch(theOrder){
-
-    //     case 'desc': {
-    //         allData = allData.sort((a, b) => {
-    //             if (b[by] > a[by]) {
-    //                 return 1;
-    //             }else if(b[by] < a[by]) {
-    //                 return -1;
-    //             }else{
-    //                 return 0;
-    //             }
-    //         })
-    //         break;
-    //     }
-
-    //     default: {
-    //        allData = allData.sort((a, b) => {
-    //             if (a[by] > b[by]) {
-    //                 return 1;
-    //             }else if(a[by] < b[by]) {
-    //                 return -1;
-    //             }else{
-    //                 return 0;
-    //             }
-    //         })
-    //     }
-    // }
-
-    // }
