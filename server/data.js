127166afdcf2b9e40db5f4cbefb66cd44c28d127
= <jacex17@gmail.com>
2020-01-18 01:53:38 -0500

    add more items to data.js

--- server/data.js
+++ server/data.js
@@ -79,5 +79,125 @@ module.exports = [
         id: 20,
         name: "my-app-020",
     },
+    {
+        id: 21,
+        name: "my-app-021",
+    },
+    {
+        id: 22,
+        name: "my-app-022",
+    },
+    {
+        id: 23,
+        name: "my-app-023",
+    },
+    {
+        id: 24,
+        name: "my-app-024",
+    },
+    {
+        id: 25,
+        name: "my-app-025",
+    },
+    {
+        id: 26,
+        name: "my-app-026",
+    },
+    {
+        id: 27,
+        name: "my-app-027",
+    },
+    {
+        id: 28,
+        name: "my-app-028",
+    },
+    {
+        id: 29,
+        name: "my-app-029",
+    },
+    {
+        id: 30,
+        name: "my-app-030",
+    },
+    {
+        id: 31,
+        name: "my-app-031",
+    },
+    {
+        id: 32,
+        name: "my-app-032",
+    },
+    {
+        id: 33,
+        name: "my-app-033",
+    },
+    {
+        id: 34,
+        name: "my-app-034",
+    },
+    {
+        id: 35,
+        name: "my-app-035",
+    },
+    {
+        id: 36,
+        name: "my-app-036",
+    },
+    {
+        id: 37,
+        name: "my-app-037",
+    },
+    {
+        id: 38,
+        name: "my-app-038",
+    },
+    {
+        id: 39,
+        name: "my-app-039",
+    },
+    {
+        id: 40,
+        name: "my-app-040",
+    },
+    {
+        id: 41,
+        name: "my-app-041",
+    },
+    {
+        id: 42,
+        name: "my-app-042",
+    },
+    {
+        id: 43,
+        name: "my-app-043",
+    },
+    {
+        id: 44,
+        name: "my-app-044",
+    },
+    {
+        id: 45,
+        name: "my-app-045",
+    },
+    {
+        id: 46,
+        name: "my-app-046",
+    },
+    {
+        id: 47,
+        name: "my-app-047",
+    },
+    {
+        id: 48,
+        name: "my-app-048",
+    },
+    {
+        id: 49,
+        name: "my-app-049",
+    },
+    {
+        id: 50,
+        name: "my-app-050",
+    },
 
     ]
